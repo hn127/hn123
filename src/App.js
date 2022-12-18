a8cf76a0c9d550ec4a4bf8ab5faa412ecef34a00
nagesh <hnn23323@gmail.com>
2022-12-18 13:11:36 +0530

    hello runshaw

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>YOUR REGISTRATION FORM</h2>
+      <form><label>
+         first name
+        <input text="fname" id="fname"></input><br></br>
+        <br></br>
+        last name
+        <input text="lname" id="lname"></input><br></br>
+        <br></br>
+        <button>submit</button>
+         </label>
+         <radio></radio>
+        </form>
+    </div>
+  );
+}
+
+export default App;
